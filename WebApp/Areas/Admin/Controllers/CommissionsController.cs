64588c451ede3b9475d9b8e45c18fd722d5aea36
nokdev123 <nokdev123@gmail.com>
2026-04-05 14:34:43 +0530

    Commission rule & in BRD URL updated

--- WebApp/Areas/Admin/Controllers/CommissionsController.cs
+++ WebApp/Areas/Admin/Controllers/CommissionsController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StandardCMS.DB;
+using System.Web.Http;
 
 namespace WebApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class CommissionsController : Controller
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    public class CommissionsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
@@ -15,10 +19,13 @@ namespace WebApp.Areas.Admin.Controllers
         }
 
         // GET: Commissions
-        public IActionResult Index()
+        [System.Web.Http.HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("GetCommissions")]
+        public IActionResult  Index()
         {
-            var commissions = _context.Commissions.Include(c => c.Agent).Include(c => c.Sale).ToList();
-            return View(commissions);
+            var commissions = _context.Commissions.ToList();//.Include(c => c.Agent).ToList();//.Include(c => c.Sale).ToList();
+            //return View(commissions);
+             return Ok(commissions);
         }
 
         // You can add Approve and Deny methods as needed
